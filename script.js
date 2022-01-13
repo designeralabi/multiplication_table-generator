8616245a10d548ff747041a9f753e59900f5d548
designeralabi <bdlkrmalabi@gmail.com>
2022-01-13 05:24:58 +0100

    fix bug in code

--- script.js
+++ script.js
@@ -2,32 +2,23 @@ const form = document.querySelector('.form-wrapper');
 const genOutput = document.querySelector('.output');
 const userNum = document.querySelector('#num');
 const resetBtn = document.querySelector('#reset-btn');
-const dataCaution = document.querySelector('#data-caution'); 
+ 
 
-var msg = '';
+
 
 form.addEventListener('submit', (evt)=>{
     evt.preventDefault();
     
+    var msg = '';
     var userInput = Number(userNum.value);
     var by = 1;
     if (typeof userInput === 'number'){
-
-        while (userInput <= 12, by <= 10 ) {
+        while (userInput <= 12, by <= 100 ) {
             msg += userInput + ` x ${by} = ` + (userInput * by + '<br />');
        
             by++;
-        }
-        
-    }else if(typeof userInput !== typeof Number()){
-        dataCaution.textContent = 'pls enter a valid number';
-    }
-    
-    
-    if (evt.target.tagName === 'SPAN'){
-        console.log('cancel button clicked');
-    }
-    
+        }   
+    }    
 
     genOutput.innerHTML = msg;
 
@@ -35,12 +26,6 @@ form.addEventListener('submit', (evt)=>{
         genOutput.innerHTML = '';
     });
     
-}, true);
-
-
-// toggle page color mode
-document.addEventListener('dblclick',()=>{
-    console.log('you double cli');
 }, false);
 
 
