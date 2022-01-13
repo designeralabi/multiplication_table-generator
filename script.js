b03e2e171b680629a415050534bdc41444e507b1
designeralabi <bdlkrmalabi@gmail.com>
2022-01-13 02:34:03 +0100

    created css variables

--- script.js
+++ script.js
@@ -2,20 +2,32 @@ const form = document.querySelector('.form-wrapper');
 const genOutput = document.querySelector('.output');
 const userNum = document.querySelector('#num');
 const resetBtn = document.querySelector('#reset-btn');
+const dataCaution = document.querySelector('#data-caution'); 
 
 var msg = '';
 
 form.addEventListener('submit', (evt)=>{
     evt.preventDefault();
     
-    var i = userNum.value;
+    var userInput = Number(userNum.value);
     var by = 1;
-    
-    while (i <= 12, by <= 100 ) {
-        msg += i + ` x ${by} = ` + (i * by + '<br />');
-   
-        by++;
+    if (typeof userInput === 'number'){
+
+        while (userInput <= 12, by <= 10 ) {
+            msg += userInput + ` x ${by} = ` + (userInput * by + '<br />');
+       
+            by++;
+        }
+        
+    }else if(typeof userInput !== typeof Number()){
+        dataCaution.textContent = 'pls enter a valid number';
     }
+    
+    
+    if (evt.target.tagName === 'SPAN'){
+        console.log('cancel button clicked');
+    }
+    
 
     genOutput.innerHTML = msg;
 
@@ -23,8 +35,12 @@ form.addEventListener('submit', (evt)=>{
         genOutput.innerHTML = '';
     });
     
-});
+}, true);
 
 
+// toggle page color mode
+document.addEventListener('dblclick',()=>{
+    console.log('you double cli');
+}, false);
 
 
